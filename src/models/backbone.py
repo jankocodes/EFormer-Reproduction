b6f3a008ec0fb013a18a9f9b80b2334a209f651d
jankocodes <schuhmann.jannis@gmail.com>
2025-01-23 23:29:38 +0100

    additional backbone output and prediction stage prototype added

--- src/models/backbone.py
+++ src/models/backbone.py
@@ -16,11 +16,11 @@ class Backbone(nn.Module):
         
         
     def forward(self, x):
-        x = self.layer1(x) #(B,256,H/4,W/4)
+        f_enc = self.layer1(x) #(B,256,H/4,W/4)
 
-        f_hr = self.layer2(x) #(B,512,H/8,W/8)
+        f_hr = self.layer2(f_enc) #(B,512,H/8,W/8)
         f_lr = self.layer3(f_hr) #(B,1024,H/16,W/16)
 
-        return f_hr, f_lr
+        return f_enc, f_hr, f_lr
     
     
--- src/models/eformer.py
+++ src/models/eformer.py
@@ -20,10 +20,20 @@ class EFormer(nn.Module):
             TransformerBlock()
         )
         
+        self.conv_semantic = nn.Conv2d(256, 256, kernel_size=3,padding=1)
+        self.conv_contour = nn.Conv2d(256, 256, kernel_size=3,padding=1)
+
+        self.conv_fuse = nn.Conv2d(256, 256, kernel_size=3,padding=1)
+        
+        self.head= nn.Conv2d(256, 1, kernel_size=3, padding=1 )
+        
     
     def forward(self, x):
+ 
+        B,_,H,W= x.shape
+        
         #resnet50 backbone
-        f_hr, f_lr= self.backbone(x) #(B,512,H/8,W/8), (B,1024,H/16,W/16)
+        f_enc, f_hr, f_lr= self.backbone(x) #(B,512,H/8,W/8), (B,1024,H/16,W/16)
         
         f_hr = self.proj_hr(f_hr) #(B,256,H/8,W/8)
         f_lr = self.proj_lr(f_lr) #(B,256,H/16,W/16)
@@ -32,21 +42,47 @@ class EFormer(nn.Module):
             f_lr, size=f_hr.shape[2:], mode='bilinear', align_corners=False #(B,256,H/8,W/8)
         )
 
-        #change dimension
-        f_hr_emb = f_hr.flatten(2).permute(2, 0, 1)  #(N,B,256)
-        f_lr_emb = f_lr_upsampled.flatten(2).permute(2, 0, 1)  #(N,B,256)
+        #(B,256,H/8,W/8) -> (N,B,256)
+        f_hr_emb = f_hr.flatten(2).permute(2, 0, 1)  
+        f_lr_emb = f_lr_upsampled.flatten(2).permute(2, 0, 1) 
         
         #transformer
         f_contour, f_semantic = f_hr_emb, f_lr_emb
         for block in self.transformer_blocks:
             f_contour, f_semantic = block(f_contour, f_semantic)
         
+        #(N,B,256) -> (B,256,N) -> (B,256,H//8,W//8)
+        f_contour= f_contour.permute(1,2,0).view(B,256,H//8,W//8)
+        f_semantic= f_semantic.permute(1,2,0).view(B,256,H//8,W//8)
+        
+        f_semantic= self.conv_semantic(f_semantic+f_lr_upsampled)
+        f_contour= self.conv_contour(f_contour+f_hr)
+        
+        #prediction stage
+        f_semantic_contour= self.conv_fuse(f_semantic+f_contour)
+        
+        fused_features= f_enc + nn.functional.interpolate(
+            f_semantic_contour, size= f_enc.shape[2:], mode= 'bilinear',align_corners=False)
+        
+        matte= self.head(fused_features)
+        
+        #upsample to original size
+        matte= nn.functional.interpolate(matte, (H,W))
+
+        
+        print(matte.min())
+        return matte
+            
+            
+        
+        
         
         
         
 
 if __name__ == "__main__":
-    dummy_input = torch.randn(2, 3, 224, 224)  # Batch of 2 images
+    dummy_input = torch.randn(1, 3, 224, 224) 
     model = EFormer()
     model(dummy_input)
+    
     
