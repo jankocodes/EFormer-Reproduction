7d86346fe28337df4c06fb594bfc364acbe2fe8a
jankocodes <schuhmann.jannis@gmail.com>
2025-04-19 20:48:29 +0200

    fixed pin memory bug

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -33,22 +33,33 @@ class EFormerDataset(Dataset):
     def __getitem__(self, idx):
         name = self.filenames[idx]
         
-        composite_path= os.path.join(self.com_dir, name)
+        composite_path = os.path.join(self.com_dir, name)
         pha_path = os.path.join(self.pha_dir, name)
 
+        # Load images
         composite = Image.open(composite_path).convert("RGB")
         pha = Image.open(pha_path).convert("L")  # Alpha is grayscale
-        
-        composite = F.to_tensor(composite).to(self.device)
-        pha = F.to_tensor(pha).to(self.device)
-        
-        #random horizontal flipping
+
+        # Perform flipping and resizing on the CPU (efficient)
         if random.random() < self.p_flip:  
             composite = F.hflip(composite)
             pha = F.hflip(pha)
-            
+        
         composite = F.resize(composite, self.size)
         pha = F.resize(pha, self.size)
-       
+
+        # Convert images to tensors (on CPU by default)
+        composite = F.to_tensor(composite)
+        pha = F.to_tensor(pha)
+        
+        # Pin memory (on CPU tensors) before moving to device (GPU)
+        composite = composite.pin_memory()
+        pha = pha.pin_memory()
+
+        # Move tensors to device (GPU)
+        composite = composite.to(self.device, non_blocking=True)
+        pha = pha.to(self.device, non_blocking=True)
+        
         return composite, pha
+
 
